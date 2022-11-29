4d2c6ec37992756fa7cbb8e692ee8cce47755b49
St-Luciferr <suntoss.pandey@gmail.com>
2022-11-29 23:03:25 +0545

    response and request

--- basicApi/urls.py
+++ basicApi/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import article_list
+from .views import *
+from rest_framework.urlpatterns import format_suffix_patterns
+
 
 
 urlpatterns = [
-    path('', article_list,name='list'),  
+    path('article/', article_list,name='list'),  
+    path('article/<int:pk>/',article_detail,name='detail')
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- basicApi/views.py
+++ basicApi/views.py
@@ -1,25 +1,49 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+# from rest_framework.parsers import JSONParser
 from .models import Article
 from .serializers import ArticleSerializers
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-@csrf_exempt
-def article_list(request):
+@api_view(['GET','POST'])
+def article_list(request,format=None):
     if request.method=='GET':
         articles=Article.objects.all()
         serializer=ArticleSerializers(articles,many=True)
-        return JsonResponse(serializer.data,safe=False)
-    if request.method=='POST':
-        data=JSONParser().parse(request)
-        serializer=ArticleSerializers(data=data)
+        return Response(serializer.data)
+    elif request.method=='POST':
+        serializer=ArticleSerializers(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data,status=201)
-        return JsonResponse(serializer.errors,status=400)
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
 
+@api_view(['GET','PUT','DELETE'])
+def article_detail(request,pk,format=None):
+    """
+    Retrieve, update or delete an article.
+    """
+    try:
+        article = Article.objects.get(pk=pk)
+    except Article.DoesNotExist:
+        return HttpResponse(status=404)
 
-# Create your views here.
+    if request.method == 'GET':
+        serializer = ArticleSerializers(article)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = ArticleSerializers(article,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
